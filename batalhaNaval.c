4e3aaa0b722bda102a40bb021ab8eb276fa0a2c3
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-17 14:04:52 +0000

    inserido os navios e as possições das casas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-
-
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
+
 int main() {
-    int tabuleiro[10][10] = {0};#include <stdio.h>
+    int tabuleiro[10][10] = {0}; // Inicializa o tabuleiro com zeros (água) 
+// Inserindo o primeiro navio na linha 4 ocupando as colunas B, C e D
+    int linhaNavio1 = 3; // Índices começam em 0
+    int inicioColuna1 = 1; // Índice 1 representa a coluna B
+
+for (int i = 0; i < 3; i++) { tabuleiro[linhaNavio1][inicioColuna1 + i] = 3;} // Insere o navio
+
+// Inserindo o segundo navio na coluna B, ocupando as linhas 7, 8 e 9
+    int colunaNavio2 = 1; // Índice 1 representa a coluna B
+    // Índices 6, 7 e 8 correspondem às linhas 7, 8 e 9
+for (int i = 6; i < 9; i++) {tabuleiro[i][colunaNavio2] = 3;} // Insere o segundo navio
+    
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
